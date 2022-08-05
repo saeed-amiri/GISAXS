eca283b69cbb87f45a21dd8210b9491aad02b4fb
Saeed Amiri <samiri@uni-bremen.de>
2022-08-05 15:04:59 +0200

    Initiation and imports

---
+++ codes/gisaxs.py
@@ -0,0 +1,16 @@
+"""
+Modeling GISAXS experiments for a 2 dimentional model.
+First:
+    Read the LAMMPS datafile
+    calculate the poits in the reciprocal space
+    ...
+"""
+import sys
+import read_lmp_data as relamp
+import reciprocal as recip
+
+fname: str = sys.argv[1]  # LAMMPS datafile to work with
+
+data = relamp.ReadData(fname)
+
+
---
+++ codes/reciprocal.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+
+
+class Doc:
+    """Calculate the reciprocal points of the structure
+    Input:
+        LAMMPS data file read by read_lmp_data
+    Output:
+        pd.DataFrame of the points reciprocal space
+    """
+
+class MakeReciprocal:
+    """Mathematical caluatations of the space transformation"""
+    def __init__(self, df: pd.DataFrame) -> None:
+        pass
+
+

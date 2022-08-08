93fdc5f16c2483650325d0b799c3fb9a6cfddbdb
Saeed Amiri <samiri@uni-bremen.de>
2022-08-08 15:39:17 +0200

    update

--- codes/reciprocal.py
+++ codes/reciprocal.py
@@ -14,7 +14,8 @@ class Doc:
 class MakeReciprocal:
     """Mathematical caluatations of the space transformation"""
     def __init__(self, df: pd.DataFrame) -> None:
-        self.df_to_array(df)
+        reals_arr: np.array = self.df_to_array(df)  # Coords in real space
+        self.to_reciprocal(reals_arr)
         del df
 
     def df_to_array(self, df: pd.DataFrame) -> np.array:
@@ -26,12 +27,18 @@ class MakeReciprocal:
         tmp_arr = df_tmp.to_numpy(dtype=float)
         rows: int  # Number of rows in the Data file==NAtoms
         cols: int  # Number of columns at each row, then subtract id
-        coords_arr: np.array  # To save the coords based on the id
+        reals_arr: np.array  # To save the coords based on the id
         rows, cols = tmp_arr.shape
-        coords_arr = np.zeros((rows, cols-1), dtype=float)
-        # print(tmp_arr)
+        reals_arr = np.zeros((rows, cols-1), dtype=float)
         for row in range(rows):
             id = int(tmp_arr[row][0]) - 1
-            coords_arr[id][0] = tmp_arr[row][1]  # x positions
-            coords_arr[id][1] = tmp_arr[row][2]  # y positions
-            coords_arr[id][2] = tmp_arr[row][3]  # z positions
+            reals_arr[id][0] = tmp_arr[row][1]  # x positions
+            reals_arr[id][1] = tmp_arr[row][2]  # y positions
+            reals_arr[id][2] = tmp_arr[row][3]  # z positions
+        del tmp_arr
+        return reals_arr
+    
+    def to_reciprocal(self, reals_arr: np.array) -> None:
+        """transfrom data to the reciprocal space"""
+        print(reals_arr)
+

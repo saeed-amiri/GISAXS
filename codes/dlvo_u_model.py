23c33db978f8d68c896633d8c6c4855ce25f08e7
Saeed Amiri <samiri@uni-bremen.de>
2022-09-12 09:08:58 +0200

    strat colloid method

--- codes/dlvo_u_model.py
+++ codes/dlvo_u_model.py
@@ -15,3 +15,29 @@ class Doc:
     Changing the values of variables shows behavior change in the main
     interaction.
     """
+
+
+class Hamaker:
+    """Hamaker energy prefactor"""
+    A_CC: float = 39.5  # 4*np.pi**2 = 39.478417 colloid-colloid
+    A_SS: float = 144.0  # solvent-solvent (if epsilon=1, so that 144/36=4)
+    A_CS: float = 5688.0  # np.sqrt(A_CC*A_SS)  colloid-solvent
+
+
+class Colloid:
+    """modeling Colloid interaction between particles"""
+    def __init__(self) -> None:
+        pass
+
+    def get_colloid(self) -> None:
+        """call all the methods to calculate the interaction"""
+        pass
+
+    def colloid_colloid(self) -> None:
+        """get colloid-colloid interaction"""
+
+    def colloid_solvent(self) -> None:
+        """get colloid_solvent interaction"""
+
+    def solvent_solvent(self) -> None:
+        """get solvent_solvent interaction"""
